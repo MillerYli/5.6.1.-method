d45427fbf7c512e8108df8b80ed7e3163f3d1e10
Yulia Miller <miller82.98@mail.ru>
2022-12-25 20:51:08 -0800

    Добавьте файлы проекта.

---
+++ 5.6.1. method/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int number = 0;
+            for (int i = 0; i < result.Length; i++)
+            { 
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        number = result[i];
+                        result[i] = result[j];
+                        result[j] = number;
+                    }
+            }
+            
+
+            for(int k = 0; k < result.Length; k++)
+            {
+                Console.Write(result[k] + " ");
+            }
+
+            return result;
+        }
+        public static void Main(string[] args) 
+        { 
+            GetArrayFromConsole();
+            
+        }
+    }
+}
